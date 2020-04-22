0b1dcb0e555f4a3fe9212e58688bd860cc61ad86
Rodrigo De La Mora <delamorarodrigo3141@yahoo.com>
2020-04-22 16:01:27 -0700

    Created buttons that use axios put and delete. Stretch Goal: Created an add movie button.

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,17 +1,21 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import MovieCard from "./MovieCard";
 
 function MovieList({ movies }) {
+  const { push } = useHistory();
   return (
-    <div className="movie-list">
-      {
-        movies.map(movie => (
-          <Link key={movie.id} to={`/movies/${movie.id}`}>
-            <MovieCard movie={movie} />
-          </Link>
-        ))
-      }
+    <div>
+      <button onClick={() => push(`/add-movie`)}>Add Movie</button>
+      <div className="movie-list">
+        {
+          movies.map(movie => (
+            <Link key={movie.id} to={`/movies/${movie.id}`}>
+              <MovieCard movie={movie} />
+            </Link>
+          ))
+        }
+      </div>
     </div>
   );
 }
